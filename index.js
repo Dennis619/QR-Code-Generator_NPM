9b067caabbfc0d2a84539bb9e16ae893be17082d
Dennis Somba <47259138+Dennis619@users.noreply.github.com>
2024-03-05 10:24:45 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+//var generateName = require('sillyname');
+
+//const superheroes = require('superheroes');
+
+import superheroes from 'superheroes';
+
+console.log("I am " + superheroes.random());
+
+import generateName from 'sillyname';
+var sillyName = generateName();
+
+console.log(`my name is ${sillyName}`);
